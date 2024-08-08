cf120cd061f96772558dcc933781c28ebd35922d
luisillo19 <luisillo19@icloud.com>
2024-08-08 17:57:37 +0200

    new update 2

--- js/menuHamburguesa.js
+++ js/menuHamburguesa.js
@@ -1,19 +1,38 @@
-document.addEventListener('DOMContentLoaded', () => {
-    function cerrarNav() {
-        const cerrar = document.getElementById('botonCerrarNav');
-        const nav = document.getElementById('headerNav');
-        const hamburgerBtn = document.getElementById('menuHamburguesa');
+// document.addEventListener('DOMContentLoaded', () => {
+//     function cerrarNav() {
+//         const cerrar = document.getElementById('botonCerrarNav');
+//         const nav = document.getElementById('headerNav');
+//         const hamburgerBtn = document.getElementById('menuHamburguesa');
 
-        cerrar.addEventListener('click', () => {
-            nav.classList.add('ocultar');
-            nav.classList.remove('mostrar');
-        });
+//         cerrar.addEventListener('click', () => {
+//             nav.classList.add('ocultar');
+//             nav.classList.remove('mostrar');
+//         });
 
-        hamburgerBtn.addEventListener('click', () => {
-            nav.classList.add('mostrar');
-            nav.classList.remove('ocultar');
-          });
-    };
+//         hamburgerBtn.addEventListener('click', () => {
+//             nav.classList.add('mostrar');
+//             nav.classList.remove('ocultar');
+//           });
+//     };
 
-    cerrarNav();
+//     cerrarNav();
+// });
+
+const cerrar = document.getElementById('botonCerrarNav');
+const nav = document.getElementById('headerNav');
+const hamburgerBtn = document.getElementById('menuHamburguesa');
+
+hamburgerBtn.addEventListener('click', () => {
+
+    nav.classList.add('mostrar');
+    nav.classList.remove('ocultar');    
+
 });
+
+cerrar.addEventListener('click', ()=> {
+
+    nav.classList.add('ocultar');
+    nav.classList.remove('mostrar');
+
+});
+
